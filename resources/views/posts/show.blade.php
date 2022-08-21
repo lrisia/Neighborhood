59664b2a08f645cbfb1d8507b04cb212e76d75ea
S-Nattapong <saw1229@hotmail.com>
2022-08-21 18:09:18 +0700

    brabrabra

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -43,10 +43,10 @@
                 &nbsp;สถานะ: {{ $post->statusTranslator() }}
             </p>
 
-            <form action="{{ route('posts.status.update', ['post' => $post->id]) }}" method="post">
+            <!-- <form action="{{ route('posts.status.update', ['post' => $post->id]) }}" method="post">
                 @csrf
 {{--                @method('PUT')--}}
-                <div class="mt-2 bg-gray-100 p-2 rounded">
+                <div class="mt-2 bg-gray-100 p-2 rounded"> -->
                     <label for="countries" class="mx-4 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">เลือกสถานะที่จะเปลี่ยน</label>
                     <select name="status" id="status" class="w-9/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 {{--                        <option value="Default" selected>เลือกสถานะ</option>--}}
